d57e567be7c8c9fd418505575942e17269c57ab6
Abhishek Oza <101032683+Ahishekoza@users.noreply.github.com>
2025-01-08 10:44:53 +0900

    Updated main.jsx Toaster as per alias resolve rule

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContextProvider.jsx";
-import { Toaster } from "./components/ui/toaster.jsx";
+import { Toaster } from "@/components/ui/toaster.jsx";
 import { TaskContextProvider } from "./context/TaskContextProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
